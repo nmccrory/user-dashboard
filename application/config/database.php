268c91f5f20b9ce73b81102ea870485c40d977b8
Nick McCrory <n.mccrory@icloud.com>
2017-06-05 12:28:18 -0700

    added DB connection info for SQL Server instance on Azure.

--- application/config/database.php
+++ application/config/database.php
@@ -64,9 +64,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
+	'hostname' => 'user-dashboard.database.windows.net',
+	'username' => 'nmccrory@user-dashboard',
+	'password' => '31337cOOkie45',
 	'database' => 'user_dashboard',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
