d6b26a254cdc1d05da841a352625e5dbeb11e82c
Nick McCrory <n.mccrory@icloud.com>
2017-06-05 15:55:28 -0700

    Styling tweak to index.php

--- application/views/index.php
+++ application/views/index.php
@@ -23,7 +23,7 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col s12 m10 offset-m1 grey lighten-2">
+				<div class="col s12 grey lighten-2">
 					<div class="col s12 m10 offset-m1">
 						<section>
 							<h1>Welcome</h1>
